f72203fcccba5baaba9598ac92f35e643f82c957
priyank2809 <priyank2806@gmail.com>
2023-09-02 15:14:52 +0530

    route fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { PricingComponent } from './pricing/pricing.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
     { path: 'features', component: FeaturesComponent },
     { path: 'pricing', component: PricingComponent },
     { path: 'contact', component: ContactComponent },
